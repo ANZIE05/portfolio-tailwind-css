440895780f2ca07e78fd41f24c80782eab044f9f
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2024-12-27 01:25:06 +0500

    Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,16 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Portfolio using Tailwind CSS",
